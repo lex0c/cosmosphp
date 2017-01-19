411b11f9d97ed5c9abc3f4b6fff4df074169f635
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-19 14:40:10 -0200

    Interface HandlerInterface renamed to FilesystemInterface

--- src/Contracts/Filesystem/FilesystemInterface.php
+++ src/Contracts/Filesystem/FilesystemInterface.php
@@ -3,15 +3,13 @@
 namespace Cosmos\Contracts\Filesystem;
 
 /**
- * Handler Interface
+ * Filesystem Interface
  * @link https://github.com/lleocastro/cosmosphp/
  * @license https://github.com/lleocastro/cosmosphp/blob/master/LICENSE
  * @author Léo Castro <leonardo_carvalho@outlook.com>
  * @package Cosmos\Contracts\Filesystem
- * @copyright 2016 - 2017 (MIT License)
- * @version 1.0.0
  */
-interface HandlerInterface
+interface FilesystemInterface
 {
     /**
      * Create a new.
