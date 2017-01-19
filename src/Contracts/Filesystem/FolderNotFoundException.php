a040831a5e65a16cfc2084716e5cc50e88883bf1
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-19 00:38:02 -0200

    Filesystem contracts

---
+++ src/Contracts/Filesystem/FolderNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cosmos\Contracts\Filesystem;
+
+/**
+ * Folder Not Found Exception
+ * @link https://github.com/lleocastro/cosmosphp/
+ * @license https://github.com/lleocastro/cosmosphp/blob/master/LICENSE
+ * @author Léo Castro <leonardo_carvalho@outlook.com>
+ * @package Cosmos\Contracts\Filesystem
+ * @copyright 2016 - 2017 (MIT License)
+ * @version 1.0.0
+ */
+class FolderNotFoundException extends \RuntimeException
+{
+    //
+}
---
+++ src/Contracts/Filesystem/HandlerInterface.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Cosmos\Contracts\Filesystem;
+
+/**
+ * Handler Interface
+ * @link https://github.com/lleocastro/cosmosphp/
+ * @license https://github.com/lleocastro/cosmosphp/blob/master/LICENSE
+ * @author Léo Castro <leonardo_carvalho@outlook.com>
+ * @package Cosmos\Contracts\Filesystem
+ * @copyright 2016 - 2017 (MIT License)
+ * @version 1.0.0
+ */
+interface HandlerInterface
+{
+    /**
+     * Create a new.
+     *
+     * @param string $name
+     * @param string $path
+     * @param bool   $force
+     *
+     * @return bool
+     *
+     * @throws \RuntimeException
+     */
+    public function create(string $name, string $path, bool $force):bool;
+
+    /**
+     * Determine if exists.
+     *
+     * @param string $name
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function exists(string $name, string $path):bool;
+
+    /**
+     * Delete at a given path.
+     *
+     * @param string $name
+     * @param string $path
+     *
+     * @return bool
+     *
+     * @throws \RuntimeException
+     */
+    public function delete(string $name, string $path):bool;
+
+    /**
+     * Copy to a new location.
+     *
+     * @param string $name
+     * @param string $to
+     * @param string $path
+     *
+     * @return bool
+     *
+     * @throws \RuntimeException
+     */
+    public function copy(string $name, string $to, string $path):bool;
+
+    /**
+     * Move to a new location.
+     *
+     * @param string $name
+     * @param string $to
+     * @param string $path
+     *
+     * @return bool
+     *
+     * @throws \RuntimeException
+     */
+    public function move(string $name, string $to, string $path):bool;
+
+    /**
+     * Look for something inside.
+     *
+     * @param string $name
+     * @param string $search
+     * @param string $path
+     *
+     * @return bool
+     *
+     * @throws \RuntimeException
+     */
+    public function searchInside(string $name, string $search, string $path):bool;
+
+}
