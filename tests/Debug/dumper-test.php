dda51c597b9146156c4369c5dce0c920195e86e3
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-18 22:17:33 -0200

    Tests for debugger component

---
+++ tests/Debug/dumper-test.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once (dirname(__DIR__) . '/bootstrap.php');
+
+$user = new stdClass();
+$user->name = 'Léo';
+$user->age = 23;
+$user->city = 'SJC';
+$user->x = [
+    'hello' => 'Hello World!',
+    'Something' => 'Yeah..',
+    'My x' => [
+        1, 2, 3, 4, 5, 6, 7
+    ]
+];
+
+dd($user);
