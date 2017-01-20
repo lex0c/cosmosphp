0401dfdc923deb88bb8ab8b27faa6adaa3f3b19a
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-20 18:33:43 -0200

    Tested methods 'add' and 'addIn' of ArrayList

---
+++ tests/Collections/Stupids/StupidClass.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests\Collections\Stupids;
+
+/**
+ * Created by PhpStorm.
+ * User: leo
+ * Date: 20/01/17
+ * Time: 15:33
+ */
+class StupidClass extends \stdClass
+{
+    public function __construct()
+    {
+        return 'Equals?';
+    }
+
+}
---
+++ tests/Collections/array-list.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once (dirname(__DIR__) . '/bootstrap.php');
+
+use \Tests\Collections\Stupids\StupidClass;
+use \Cosmos\Collections\ArrayList;
+
+$list = new ArrayList();
+
+$list->add(12);
+$list->add('hello');
+$list->add('15');
+$list->add(11.23);
+$list->add(new StupidClass());
+
+echo "<pre>";
+print_r($list->getAll());
+echo "</pre>";
+
+echo "<br>";
+echo "<br>";
+
+$list->addIn(2, 'world');
+$list->addIn(7, 'aaaaa');
+$list->addIn(6, 4365676);
+
+echo "<pre>";
+print_r($list->getAll());
+echo "</pre>";
+
