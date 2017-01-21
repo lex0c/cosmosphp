e9200cacb207516b14325d5ff8114cf131c6d0ab
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-21 02:06:44 -0200

    Handles exception for invalid or non-existent pointer

--- src/Collections/Exceptions/NullPointerException.php
+++ src/Collections/Exceptions/NullPointerException.php
@@ -10,5 +10,16 @@ namespace Cosmos\Collections\Exceptions;
  */
 class NullPointerException extends \RuntimeException
 {
-    //
+    /**
+     * Handles exception for invalid or non-existent pointer.
+     *
+     * @param string    $message
+     * @param    int    $code
+     * @param \Exception $previous
+    */
+    public function __construct(string $message = '', int $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
 }
