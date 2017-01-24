95c75253a7f67762b7ffb7ce11758aa752d259d2
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-23 23:31:12 -0200

    Mask for dual hash encryption

---
+++ src/Security/Traits/Mask.php
@@ -0,0 +1,44 @@
+<?php
+namespace Cosmos\Security\Traits;
+
+/**
+ * Mask
+ * @link https://github.com/lleocastro/cosmosphp/
+ * @license https://github.com/lleocastro/cosmosphp/blob/master/LICENSE
+ * @author Léo Castro <leonardo_carvalho@outlook.com>
+ * @package \Cosmos\Security\Traits
+ */
+trait Mask
+{
+    /**
+     * Hard hash encrypting.
+     *
+     * @param string $encryptedData
+     *
+     * @return string
+     */
+    protected function inverse(string $encryptedData):string
+    {
+        return base64_encode(strrev(
+                substr($encryptedData, (strlen($encryptedData)/2)-strlen($encryptedData), strlen($encryptedData)).
+                substr($encryptedData, 0, (strlen($encryptedData)/2)-strlen($encryptedData)))
+        );
+    }
+
+    /**
+     * Decrypting.
+     *
+     * @param string $encryptedData
+     *
+     * @return string
+     */
+    protected function reverse(string $encryptedData):string
+    {
+        $encryptedData = base64_decode($encryptedData);
+        return strrev(
+            substr($encryptedData, (strlen($encryptedData)/2)-strlen($encryptedData),strlen($encryptedData)).
+            substr($encryptedData, 0, (strlen($encryptedData)/2)-strlen($encryptedData))
+        );
+    }
+
+}
